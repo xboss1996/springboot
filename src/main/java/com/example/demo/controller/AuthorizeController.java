ed389519f39d82b146be103db5579f53f08cdf86
xiao <491712057@qq.com>
2019-09-10 18:01:27 +0800

    login

--- src/main/java/com/example/demo/controller/AuthorizeController.java
+++ src/main/java/com/example/demo/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
 
@@ -21,7 +23,8 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String uri;
     @GetMapping("/callback")
-    public String callback(@RequestParam(name="code") String code,@RequestParam(name="state") String state)
+    public String callback(@RequestParam(name="code") String code, @RequestParam(name="state") String state,
+                           HttpServletRequest request)
     {
         AccessTokenDTO accesstokenDTO=new AccessTokenDTO();
         accesstokenDTO.setCode(code);
@@ -31,8 +34,15 @@ public class AuthorizeController {
         accesstokenDTO.setClient_secret(secret);
         String accessToken = githubProvider.getAccessToken(accesstokenDTO);
         GithubUser user =githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if(user!=null){
+            //登录成功，写cookie和Session
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        }else{
+            //登录失败，重新登录
+            return "redirect:/";
+        }
+
     }
 
 }
